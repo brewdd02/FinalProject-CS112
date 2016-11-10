8ff585a40ad4b63c5b7682d05a1258dfdba8b182
Daniel Brewer <brewdd02@students.ipfw.edu>
2016-11-10 09:55:54 -0500

    Added interface to the application page. Currently non-functional

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,3 +1,42 @@
 /**
  * Created by Daniel on 10/27/2016.
  */
+ numCourses = 1;
+
+ function addClass()
+ {
+	numCourses = numCourses + 1;
+	
+	var coursesTable = document.getElementById("tblCourses");
+	var newRow = coursesTable.insertRow(-1);
+	var colSemester = newRow.insertCell(0);
+	var colCourseId = newRow.insertCell(1);
+	
+	colSemester.innerHTML = "<input type='text' id='txtCourseSem" + numCourses + "'>";
+	colCourseId.innerHTML = "<input type='text' id='txtCourseId" + numCourses + "'>";
+ }
+ 
+ function remClass()
+ {
+	if (numCourses > 0)
+	{
+		numCourses = numCourses - 1;
+	
+		document.getElementById("tblCourses").deleteRow(-1);
+	}
+	else
+	{
+		alert("Unable to delete row");
+	}
+
+ }
+ 
+ function validate()
+ {
+	
+ }
+ 
+ function submitForm(name, email, stuId, )
+ {
+	
+ }
